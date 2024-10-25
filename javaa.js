72073b59d454c51d18207d5da73d1eb0cb64e9d6
sander1222 <sander122.kodehode@gmail.com>
2024-10-25 10:44:57 +0200

    Add files via upload

---
+++ javaa.js
@@ -0,0 +1,67 @@
+//onsole.log("Hello World, ,from java");
+
+//let x = 2;
+//const y = 3;
+
+//console.log("her er verdien av x: " + 2);
+
+// let FirstName = "Berit";
+
+// console.log(`Min mors navn er ${FirstName}`);
+
+// let lives = 2;
+
+// if (lives > 0) {
+//   console.log("Not ur time to go just yet");
+// } else if (lives === 1) {
+//   console.log("ur oneshot");
+// } else {
+//   console.log("ur dead");
+// }
+
+// const userName = "bob";
+// let userAge = 21;
+
+// let userIsAdmin = false;
+// let userIsLoggIn = false;
+// let userIsBlocked = false;
+
+// let goToPage = "";
+
+// if (!userName == "" && userAge >= 18 && !userIsBlocked && !userIsAdmin) {
+//   userIsLoggIn = true;
+//   goToPage = "/home";
+// } else if (userIsAdmin) {
+//   userIsAdmin = true;
+//   goToPage = "/admin";
+// } else {
+//   goToPage = "/logIn";
+// }
+// console.log(goToPage);
+
+// let isMember = true;
+
+// console.log(isMember ? "$2" : "$5");
+
+// let phrase = "Cool kids club";
+
+// console.log(phrase.endsWith("cool"));
+
+// let num = 10;
+
+// console.log(num * 10);
+
+// console.log(Math.round(Math.random() * 1000));
+
+console.log(Math.abs(-12));
+
+console.log(Math.max(5, 19));
+
+// let number = Math.floor(Math.random() * 100);
+
+// if (number < 50) {
+//   console.log("The number is less than 50!");
+// } else if (number > 50) {
+//   console.log("The number is greater than 50!");
+// } else {
+//   console.log("The number is exactly 50!");
